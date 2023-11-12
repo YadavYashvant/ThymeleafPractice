d73c10b4372756ed1ae5941f990fac743ae95194
yashvant <yashvantyadav855@gmail.com>
2023-11-12 09:51:34 +0530

    changing html content with thymeleaf

--- src/main/java/com/yashvant/thymeleafpractice/ThymeleafPracticeApplication.java
+++ src/main/java/com/yashvant/thymeleafpractice/ThymeleafPracticeApplication.java
@@ -2,6 +2,7 @@ package com.yashvant.thymeleafpractice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.ui.Model;
 
 @SpringBootApplication
 public class ThymeleafPracticeApplication {
--- src/main/java/com/yashvant/thymeleafpractice/controller/MyController.java
+++ src/main/java/com/yashvant/thymeleafpractice/controller/MyController.java
@@ -1,14 +1,19 @@
 package com.yashvant.thymeleafpractice.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MyController {
 
     @GetMapping("/about")
-    public String about() {
+    public String about(Model model) {
         System.out.println("Inside about() method");
+
+        model.addAttribute("name", "Yashvant Yadav");
+        model.addAttribute("currentDate", new java.util.Date().toString());
+
         return "about";
         //about.html
     }
