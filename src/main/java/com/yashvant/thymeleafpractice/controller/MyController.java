0d3d3d3a17d72c3f138604a084b83f9b4b1334f5
yashvant <yashvantyadav855@gmail.com>
2023-11-12 09:34:03 +0530

    creating controller

--- src/main/java/com/yashvant/thymeleafpractice/controller/MyController.java
+++ src/main/java/com/yashvant/thymeleafpractice/controller/MyController.java
@@ -1,4 +1,15 @@
 package com.yashvant.thymeleafpractice.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MyController {
+
+    @GetMapping("/about")
+    public String about() {
+        System.out.println("Inside about() method");
+        return "about";
+        //about.html
+    }
 }
